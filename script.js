72c13476db0f33ce0e409d51c8602f780e5d61b6
ZAINAB KHAN <146185273+zynbkhan@users.noreply.github.com>
2024-08-17 20:58:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let type = new Typed(".text", {
+    strings: ["Frontend Developer", "HTML Expert", "CSS Expert", "JavaScript Expert"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
